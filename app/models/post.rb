104ce85fcafa8a385d0b1a8c83efd0c166f26b93
James McCann <jamesredux@gmail.com>
2018-09-17 20:25:16 +0700

    add post model

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :content, presence: true, length: { minimum: 4 }
+  validates :user_id, presence:true
+end
